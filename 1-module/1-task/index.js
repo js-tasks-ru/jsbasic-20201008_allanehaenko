9a702516a996bd175ecf0e87c5484ac3c2b47c22
Nehaenko <allanehaenko@gmail.com>
2020-10-13 20:18:35 +0300

    added first task of the first theme

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let result = 1 ;
+  while(n){
+    if ((n) == 0 || (n) == 1)
+      break ;
+    result = result * n;
+    n = n - 1
+   }
+  return result;
 }
