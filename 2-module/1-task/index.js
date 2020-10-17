4905e2fbcd2f656d831abdb7db69f30b1bc73292
Nehaenko <allanehaenko@gmail.com>
2020-10-17 10:48:57 +0300

    added fisrt task module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,5 +4,13 @@
  * @returns {Number}
  */
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  delete salaries.month;
+  delete salaries.currency;
+  delete salaries.isPayed;
+  for (let salary of Object.values(salaries)) {
+    sum += salary;
+  }
+
+  return sum;
 }
