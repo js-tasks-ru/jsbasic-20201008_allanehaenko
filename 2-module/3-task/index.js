4561e85ab13401a918e19ab6f1c36562905cdea1
Nehaenko <allanehaenko@gmail.com>
2020-10-17 15:08:28 +0300

    added third task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = 3;
+    this.b = 5;
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
