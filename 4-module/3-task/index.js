9c8cc6260810d1ab708ecd6834dabe43e6956112
Nehaenko <allanehaenko@gmail.com>
2020-10-27 21:28:16 +0200

    commit for pull

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -2,5 +2,28 @@
  * Метод устанавливает необходимые по условию аттрибуты таблице
  * @param {Element} table
  */
+
 function highlight(table) {
+    let rows = table.querySelectorAll("tr");
+    rows.cells.each(function(item){
+        let cellsAtr = item.getAttribute('data-available');
+        if (cellsAtr == 'true') {
+            table.classList.add('available');
+        } else if(cellsAtr == 'false') {
+            item.classList.add('unavailable');
+        } 
+    });
 }
+
+// function highlight(table) {
+//     let cells = table.querySelectorAll("td");
+//     cells.forEach(function(item){
+//         let cellsAtr = item.getAttribute('data-available');
+//         if (cellsAtr == 'true') {
+//             table.classList.add('available');
+//         } else if(cellsAtr == 'false') {
+//             item.classList.add('unavailable');
+//         } 
+//     });
+// }
+
